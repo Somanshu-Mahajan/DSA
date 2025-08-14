73f7a504b1ec6d99c1fc1fc0c696d4e63cc4c45c
somanshu5011 <somanshumahajan@gmail.com>
2025-08-14 18:57:03 +0530

    nothing

---
+++ Data Structures/BinarySearchTree.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Node{
+    int key;
+    Node left, right;
+
+    Node(int key){
+        this.key = key;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public final class BinarySearchTree{
+    Node root;
+
+    BinaryTree(){ root = null; }
+
+
+}
